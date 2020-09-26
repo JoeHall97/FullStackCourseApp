4dd09649a76bdd7aecfc3e325aa12bc0bba6892e
Joseph Hall <josephdahall@hotmail.co.nz>
2020-09-26 20:47:12 +1200

    Set up for heroku deployment.

--- index.js
+++ index.js
@@ -26,5 +26,16 @@ mongoose.connect(keys.mongoURI);
 require('./routes/authRoutes')(app); 
 require('./routes/billingRoutes')(app);
 
+if(process.env.NODE_ENV === 'production') {
+    // Express will server up production assest, e.g. out main.js file
+    app.use(express.static('client/build'));
+
+    // Express will server up index.html if it doesn't recognise the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT);
