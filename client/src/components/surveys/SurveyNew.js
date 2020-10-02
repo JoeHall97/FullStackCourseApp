97f8a8ecc496d9e65bc318c9a0ec7882cb22ad7f
Joseph Hall <josephdahall@hotmail.co.nz>
2020-10-02 17:23:34 +1300

    Added some of the implementation for the front-end survey creation. Need to finish validation and submition implementation.

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SurveyFrom from './SurveyForm';
+
+class SurveyNew extends Component {
+  render() {
+    return (
+      <div>
+        <SurveyFrom />
+      </div>
+    );
+  }
+}
+
+export default SurveyNew;
